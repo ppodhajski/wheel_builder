1c1f8eb2a75c0fdf2c0165ed7cccf17c85b73ef2
Pawel Jozef Podhajski <pawel.podhajski@epfl.ch>
2020-12-01 10:29:44 +0100

    fix: copy repaired wheel

--- build_wheel.sh
+++ build_wheel.sh
@@ -11,7 +11,6 @@ mkdir /opt/dist
 source /opt/rh/rh-python36/enable
 pip3.6 install cmake 
 
-
 python_versions=( "/opt/rh/rh-python36/root/usr/" "/opt/py37" "/opt/py38")
 
 for i in "${python_versions[@]}"
@@ -24,8 +23,8 @@ do :
   LD_LIBRARY_PATH=$PYTHON_ROOT_PATH/lib:$LD_LIBRARY_PATH  $PYTHON_ROOT_PATH/bin/pip3 install numpy wheel
   LD_LIBRARY_PATH=$PYTHON_ROOT_PATH/lib:$LD_LIBRARY_PATH  $PYTHON_ROOT_PATH/bin/python3 setup.py bdist_wheel
   auditwheel repair /Brion/dist/*.whl
-  mv /Brion/dist/*.whl /opt/dist
-  rm build -rf
+  mv wheelhouse/*.whl /opt/dist
+  rm dist build wheelhouse -rf
 done
 
 
